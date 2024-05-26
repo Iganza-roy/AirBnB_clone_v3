27c25e88b578d9595c37fb077553eba06f420dd6
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-05-26 22:57:46 +0300

    api status

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts API and registers blueprint"""
+
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def terminate_storage(exception=None):
+    """Closes storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
