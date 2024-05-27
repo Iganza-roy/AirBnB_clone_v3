bc7eeac6d15fe1f646e348c6f278e42e45978b8d
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-05-27 09:57:01 +0300

    Some stats task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
 """Creates a route for API status"""
-
+ 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+
+classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,
+    }
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves number of objects"""
+    obj_stats = {}
+
+    for key, value in classes.items():
+        obj_stats[key] = storage.count(value)
+
+    return jsonify(obj_stats)
