ad33e5f3df6500260edff1d68303f39f8958a507
Iganza-roy <iganzaroy55@gmail.com>
2024-05-28 15:58:05 -0700

    Add tests/test_models/test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -86,7 +86,7 @@ class TestBaseModel(unittest.TestCase):
         inst1 = BaseModel()
         toc = datetime.now()
         self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(0.1)
+        time.sleep(1e-4)
         tic = datetime.now()
         inst2 = BaseModel()
         toc = datetime.now()
